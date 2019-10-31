0deee92763042b5cbc79b86bf2159213424e513d
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-10-31 09:00:04 -0300

    creating user loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello to test a ' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'teste@email.teste',
+    password_hash: '12356464',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
