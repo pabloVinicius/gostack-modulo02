945728de57d79dd64ec0589cbc9b67973869f621
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-11-02 10:31:41 -0300

    adding authorization middleware and user update method

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { SessionController, UserController } from './app/controllers';
+import { authMiddleware } from './app/middlewares';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
